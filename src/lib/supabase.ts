b32b1d72ee5247523a3e0132dfd7b44a4056e33c
AsifNawaz <asifnawazrollno48@gmail.com>
2026-09-19 09:42:13 +0500

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,7 +5,7 @@ const supabaseUrl =
   'https://0ec90b57d6e95fcbda19832f.supabase.co';
 const supabaseAnonKey =
   import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoZnVpZnVrZWJmbmh4dmxtaW5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3NzIzMzMsImV4cCI6MjEwNTM0ODMzM30.intO7_U4eSU60bCtqZcaKOOtOmVg2UL-Q8VpHTSBLgg';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
